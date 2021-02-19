883f7f06d3b2edf3da4d0b2600ce7efcfe400700
KTheXIII <pratchaya.k.git@gmail.com>
2021-02-19 15:24:27 +0100

    Added basic project files
    Added README.md
    + Instructions

---
+++ src/main/java/client/StartClient.java
@@ -0,0 +1,12 @@
+package client;
+
+/**
+ * StartClient
+ *
+ * @version 1.0
+ */
+public class StartClient {
+    public static void main(String[] args) {
+        System.out.println("Hello, Client!");
+    }
+}
