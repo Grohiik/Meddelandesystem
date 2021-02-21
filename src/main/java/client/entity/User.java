5d223d909e8e81937af678585f11a043731cd1c8
SpaceCrab <lundin.eric@hotmail.com>
2021-02-21 15:34:26 +0100

    created entity package and User class placed in entity package

---
+++ src/main/java/client/entity/User.java
@@ -0,0 +1,14 @@
+package client.entity;
+
+import javax.swing.*;
+
+public class User
+{
+    private String username;
+    ImageIcon image;
+
+    public User(String username, ImageIcon image){
+        this.image = image;
+        this.username = username;
+    }
+}
