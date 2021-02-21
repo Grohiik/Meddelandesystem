5024780565562b603089bca5cf1a273300a2bc01
SpaceCrab <lundin.eric@hotmail.com>
2021-02-21 15:55:29 +0100

    added getters setter, hashCode and equals methods father forgive me for my scuffed formatting

--- src/main/java/shared/entity/User.java
+++ src/main/java/shared/entity/User.java
@@ -11,12 +11,51 @@ public class User
     ImageIcon image;
 
     /**
-     *
      * @param username
      * @param image
      */
-    public User(String username, ImageIcon image){
+    public User(String username, ImageIcon image) {
         this.image = image;
         this.username = username;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public ImageIcon getImage()
+    {
+        return image;
+    }
+
+    public void setImage(ImageIcon image)
+    {
+        this.image = image;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    /**
+     *
+     * @return returns a hascode of the username String
+     */
+    public int hashCode(){
+        return username.hashCode();
+    }
+
+    /**
+     *
+     * @param obj
+     * @return
+     */
+    public boolean equals(Object obj){
+        if(obj != null && obj instanceof User){
+            return username.equals(((User)obj).getUsername());
+        }
+        return false;
     }
 }
