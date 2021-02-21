20de1a530099d653592160288e64e9a651fcba58
SpaceCrab <lundin.eric@hotmail.com>
2021-02-21 15:36:32 +0100

    created entity package and User class placed in entity package added javadoc comments

--- src/main/java/client/entity/User.java
+++ src/main/java/client/entity/User.java
@@ -2,11 +2,19 @@ package client.entity;
 
 import javax.swing.*;
 
+/**
+ * The user class is used to identify users and direct messages to the right client/clients
+ */
 public class User
 {
     private String username;
     ImageIcon image;
 
+    /**
+     *
+     * @param username
+     * @param image
+     */
     public User(String username, ImageIcon image){
         this.image = image;
         this.username = username;
