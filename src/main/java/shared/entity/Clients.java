b9ddf4cfe0be1e1eddbb6b161bf7868f7db7e86c
Mackan <76006738+MrMackan@users.noreply.github.com>
2021-02-21 18:01:17 +0100

    Created clients class
    Added the given code by the teachers and few comments. Will develop further but need some of the other classes to continue as it seems. So some errors shown aren't wrong but require some other functions to be "ok"

---
+++ src/main/java/shared/entity/Clients.java
@@ -0,0 +1,32 @@
+package shared.entity;
+
+import java.util.HashMap;
+
+/**
+ * Class that handles all Clients, might be moved to another direcroty
+ */
+public class Clients
+{
+    private HashMap<User, Client> clients; //TODO complete this, might need more of the code layout to fully complete
+    //missing some additions
+
+    /**
+     * says error due to sync but should be sync
+     * @param user which user it is
+     * @param client which client it is
+     */
+    public synchronized put(User user, Client client) {
+        clients.put(user, client);
+    }
+
+    /**
+     * getter
+     * @param user Which user account
+     * @return the user it is
+     */
+    public synchronized Client get(User user) {
+        return get(user);
+    }
+
+    //TODO more sync methods
+}
