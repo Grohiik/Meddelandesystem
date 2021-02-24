cc966b03b4712beef2206611723491647f3e0aea
Heart <heartbrrrrr@protonmail.com>
2021-02-24 14:07:09 +0100

    Comments and small changes
    Added javadocs comments
    Added a flush when writing too disk

--- src/main/java/client/entity/ContactList.java
+++ src/main/java/client/entity/ContactList.java
@@ -5,10 +5,20 @@ import java.util.HashMap;
 import java.util.Set;
 import shared.entity.User;
 
+/**
+ * A hashmap with contacts saved on disk.
+ *
+ * @author Linnéa Mörk
+ */
 public class ContactList {
     private HashMap<String, User> contacts;
     private String filename;
 
+    /**
+     * Initializes the hashmap with what is stored on disk.
+     * If the file does not exist, it creates it and initializes an empty hashmap.
+     * @param filename The file to load.
+     */
     public ContactList(String filename) {
         this.filename = filename;
         try {
@@ -28,21 +38,36 @@ public class ContactList {
         }
     }
 
+    /**
+     * Returns the {@link User} keyed to the user name.
+     * @param name user name you want to fetch a {@link User} for.
+     * @return The {@link User}
+     */
     public User get(String name) {
         return contacts.get(name);
     }
 
+    /**
+     * Puts a new user into the hashmap, and rewrites to disk
+     * @param name The user name, used as key in the hashmap.
+     * @param user The {@link User} object, used as value in the hashmap.
+     */
     public void put(String name, User user) {
         contacts.put(name, user);
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(filename);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
             objectOutputStream.writeObject(contacts);
+            objectOutputStream.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Returns the key in the hashmap.
+     * @return The key in the hashmap.
+     */
     public Set<String> getMap() {
         return contacts.keySet();
     }
