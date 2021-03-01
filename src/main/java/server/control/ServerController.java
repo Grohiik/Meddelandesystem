1acd0eddaa354f672d1c962a669093b476797e3b
Mackan <76006738+MrMackan@users.noreply.github.com>
2021-03-01 14:48:48 +0100

    Implemented MessageListener and MessageSender
    Not fully operational I believe. It needs more details as well in the code.

--- src/main/java/server/control/ServerController.java
+++ src/main/java/server/control/ServerController.java
@@ -1,23 +1,59 @@
 package server.control;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 import java.util.LinkedList;
 import server.entity.Buffer;
+import server.entity.UnsentMessages;
+import shared.entity.Client;
+import shared.entity.Clients;
 import shared.entity.Message;
+import shared.entity.User;
 
 /**
+ * ServerController that controls the server side
  * @author Marcus Linné, Christian Heisterkamp
  * @version 1.0
  */
 public class ServerController {
-    Buffer<Message> receivedMessages;
-    LinkedList<MessageListener> serverSocketObject;
+    private Buffer<Message> receivedMessages;
+    private LinkedList<MessageListener>
+        connectedClientList; // Is created when clients tries to connect
+    private Clients clients;
+    private int port;
 
-    public ServerController() {}
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-    // TODO ServerSocketListener, with new thread and tcp, uses arraylist to keep track of objects
+    public void startServer() {
+        ServerSocketListener serverSocketListener = new ServerSocketListener(port);
+        serverSocketListener.start();
+    }
+
+    /**
+     * Used to turn off the server by closing the serverSocket
+     * @param serverSocket
+     */
+    public void stopServer(ServerSocket serverSocket) {
+        try {
+            serverSocket.close();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    // TODO ServerSocketListener, with new thread and tcp, uses LinkedList to keep track of objects
+    /**
+     * Sub class that listens to incoming connections to the server.
+     * All connections receive a personal server thread.
+     * Transfers accepted incoming connections to MessageListener.
+     */
     private class ServerSocketListener extends Thread {
         int port;
 
@@ -30,14 +66,19 @@ public class ServerController {
             try (ServerSocket serverSocket = new ServerSocket(port)) {
                 Socket socket;
 
-                while(!interrupted()) {
-                    socket= serverSocket.accept();
+                clients = new Clients();
 
+                if (connectedClientList == null) {
+                    connectedClientList = new LinkedList<>();
                 }
-            }
-            catch (IOException e){
+
+                while (!interrupted()) {
+                    socket = serverSocket.accept();
+                    MessageListener messageListener = new MessageListener(socket);
+                    connectedClientList.add(messageListener);
+                }
+            } catch (IOException e) {
                 e.printStackTrace();
-                System.err.println("not working");
             }
         }
         // listens for a tcp connection and creates a new thread for that connection.
@@ -51,8 +92,43 @@ public class ServerController {
         // adds a received timestamp first
     }
 
-    // TODO messageSender, tries to get msg from buf and check if on or not using message listener
-    private class MessageSender {
+    // TODO messageSender, tries to get msg from buffer and check if on or not using message
+    // listener
+    // USE RECEIVE TIME
+    private class MessageSender extends Thread {
+        private ObjectOutputStream objectOutputStream;
+        private Socket socket;
+        private User user;
+
+        public MessageSender(User user, Socket socket) {
+            this.user = user;
+            this.socket = socket;
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+                if (!receivedMessages.isEmpty()) {
+                    while (!interrupted()) {
+                        Client client = new Client();
+                        Message message = receivedMessages.get();
+                        message.setReceiveTime(new Date());
+
+                        if (client.getIsOnline() == true) {
+                            objectOutputStream.writeObject(message);
+                        } else {
+                            UnsentMessages unsentMessages = new UnsentMessages();
+                            unsentMessages.put(user, message);
+                        }
+                    }
+                }
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         // tries too get messages from a buffer.
         // checks if the user it is meant for is online
         // if it is it sends the message using MessageListener's socket (THIS IS LIKELY TO BREAK TRY
--- src/main/java/server/entity/Buffer.java
+++ src/main/java/server/entity/Buffer.java
@@ -2,20 +2,16 @@ package server.entity;
 
 import java.util.LinkedList;
 
-public class Buffer<T>
-{
+public class Buffer<T> {
     private LinkedList<T> buffer = new LinkedList<T>();
 
-    public synchronized void put(T obj)
-    {
+    public synchronized void put(T obj) {
         buffer.addLast(obj);
         notifyAll();
     }
 
-    public synchronized T get() throws InterruptedException
-    {
-        while (buffer.isEmpty())
-        {
+    public synchronized T get() throws InterruptedException {
+        while (buffer.isEmpty()) {
             wait();
         }
         return buffer.removeFirst();
