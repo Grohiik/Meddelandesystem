49260be2e0bb6b3c419a2893658fabe1dcfbccc3
Heart <heartbrrrrr@protonmail.com>
2021-03-02 17:04:51 +0100

    Added UnsentMessages and Buffer in server entity

---
+++ src/main/java/server/entity/Buffer.java
@@ -0,0 +1,23 @@
+package server.entity;
+
+import java.util.LinkedList;
+
+public class Buffer<T> {
+    private LinkedList<T> buffer = new LinkedList<T>();
+
+    public synchronized void put(T obj) {
+        buffer.addLast(obj);
+        notifyAll();
+    }
+
+    public synchronized T get() throws InterruptedException {
+        while (buffer.isEmpty()) {
+            wait();
+        }
+        return buffer.removeFirst();
+    }
+
+    public int size() {
+        return buffer.size();
+    }
+}
